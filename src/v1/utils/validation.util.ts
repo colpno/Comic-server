c721c12f7a8af3020612d8d4f5e166f9419fa66c
Colpno <gvinhh@gmail.com>
2024-10-21 15:49:26 +0700

    refactor

--- src/v1/utils/validation.util.ts
+++ src/v1/utils/validation.util.ts
@@ -1,6 +1,6 @@
-import Joi, { ValidationResult } from 'joi';
-import { escape } from 'lodash';
+import { ValidationResult } from 'joi';
 
+import { Joi } from '../configs/joi.conf';
 import { HTTP_400_BAD_REQUEST } from '../constants/httpCode.constant';
 import { FailedResponseContent, GetRequestArgs } from '../types/api.type';
 
@@ -16,20 +16,16 @@ export const processValidationError = (error: Exclude<ValidationResult['error'],
   return content;
 };
 
-export const escapeHTML = (str: string): string => escape(str);
-
-export const primitiveSchema = [Joi.string(), Joi.number(), Joi.boolean()];
-
 export const normalOperatorsSchema = {
   like: Joi.string(),
-  eq: primitiveSchema,
-  ne: primitiveSchema,
+  eq: Joi.alternatives().try(Joi.string(), Joi.number(), Joi.boolean()),
+  ne: Joi.alternatives().try(Joi.string(), Joi.number(), Joi.boolean()),
   exists: Joi.boolean(),
-  gt: [Joi.string(), Joi.number()],
-  gte: [Joi.string(), Joi.number()],
-  lt: [Joi.string(), Joi.number()],
-  lte: [Joi.string(), Joi.number()],
-  size: [
+  gt: Joi.alternatives().try(Joi.string(), Joi.number()),
+  gte: Joi.alternatives().try(Joi.string(), Joi.number()),
+  lt: Joi.alternatives().try(Joi.string(), Joi.number()),
+  lte: Joi.alternatives().try(Joi.string(), Joi.number()),
+  size: Joi.alternatives().try(
     Joi.number(),
     Joi.object({
       ne: Joi.number(),
@@ -37,27 +33,34 @@ export const normalOperatorsSchema = {
       gt: Joi.number(),
       lte: Joi.number(),
       lt: Joi.number(),
-    }).or('ne', 'gte', 'gt', 'lte', 'lt'),
-  ],
-  all: Joi.array().items(...primitiveSchema),
-  in: Joi.array().items(...primitiveSchema),
-  nin: Joi.array().items(...primitiveSchema),
+    })
+  ),
+  all: Joi.array().items(Joi.alternatives().try(Joi.string(), Joi.number())),
+  in: Joi.array().items(Joi.alternatives().try(Joi.string(), Joi.number())),
+  nin: Joi.array().items(Joi.alternatives().try(Joi.string(), Joi.number())),
 };
 
 export const logicalOperatorsSchema = {
-  or: Joi.object().pattern(/.*/, normalOperatorsSchema),
-  and: Joi.object().pattern(/.*/, normalOperatorsSchema),
+  or: Joi.array().items(
+    Joi.object().pattern(/.*/, Joi.alternatives().try(Joi.string(), normalOperatorsSchema))
+  ),
+  and: Joi.array().items(
+    Joi.object().pattern(/.*/, Joi.alternatives().try(Joi.string(), normalOperatorsSchema))
+  ),
 };
 
-export const clientProvidedMongoOperatorsSchema =
+const clientProvidedMongoOperatorsSchema =
   Joi.object(normalOperatorsSchema).keys(logicalOperatorsSchema);
 
-const embedSchema = Joi.object({
+const embedSchema = {
   path: Joi.string().required(),
   select: Joi.string(),
-  match: Joi.object().pattern(/.*/, clientProvidedMongoOperatorsSchema),
+  match: Joi.object().pattern(
+    /.*/,
+    Joi.alternatives().try(Joi.string(), clientProvidedMongoOperatorsSchema)
+  ),
   populate: Joi.link('...'),
-});
+};
 export const validateGetRequest: Record<keyof GetRequestArgs, any> = {
   _limit: Joi.number().integer().positive(),
   _page: Joi.number().integer().positive(),
--- src/v1/validations/mangadex.validation.ts
+++ src/v1/validations/mangadex.validation.ts
@@ -1,36 +1,31 @@
 import { NextFunction, Request, Response } from 'express';
-import Joi, { ValidationOptions } from 'joi';
 
+import { Joi } from '../configs/joi.conf';
 import { HTTP_400_BAD_REQUEST } from '../constants/httpCode.constant';
 import { GetChapterRequest, GetMangaRequest } from '../controllers/mangadex.controller';
 import { Manga } from '../types/mangadex.type';
 import { processValidationError, validateGetRequest } from '../utils/validation.util';
 
-const includes: GetMangaRequest['query']['includes'] = ['author', 'artist', 'cover_art', 'manga'];
-const statuses: Manga['status'][] = ['ongoing', 'completed', 'hiatus', 'cancelled'];
-const contentRating: Manga['contentRating'][] = ['suggestive'];
-const states: Manga['state'][] = ['published'];
-
 export const validateGetManga = (req: Request, res: Response, next: NextFunction) => {
+  const includes: GetMangaRequest['query']['includes'] = ['author', 'artist', 'cover_art', 'manga'];
+  const statuses: Manga['status'][] = ['ongoing', 'completed', 'hiatus', 'cancelled'];
+  const contentRating: Manga['contentRating'][] = ['suggestive'];
+  const states: Manga['state'][] = ['published'];
+
   const schema = Joi.object<GetMangaRequest['query']>({
     _limit: validateGetRequest._limit,
     _page: validateGetRequest._page,
-    includes: Joi.string().valid(includes),
+    includes: Joi.string().valid(...includes),
     title: Joi.string(),
     lastVolume: Joi.string(),
     lastChapter: Joi.string(),
-    status: Joi.string().valid(statuses),
+    status: Joi.string().valid(...statuses),
     year: Joi.number().integer().positive().greater(1900).less(2100),
-    contentRating: Joi.string().valid(contentRating),
-    state: Joi.string().valid(states),
+    contentRating: Joi.string().valid(...contentRating),
+    state: Joi.string().valid(...states),
   });
 
-  const options: ValidationOptions = {
-    abortEarly: false,
-    stripUnknown: true,
-  };
-
-  const { error, value } = schema.validate(req.query, options);
+  const { error, value } = schema.validate(req.query);
 
   if (error) {
     return res.status(HTTP_400_BAD_REQUEST).json(processValidationError(error));
@@ -40,28 +35,23 @@ export const validateGetManga = (req: Request, res: Response, next: NextFunction
   }
 };
 
-const includesChapter: GetChapterRequest['query']['include'] = [
-  'emptyPages',
-  'futurePublishAt',
-  'externalUrl',
-];
-
 export const validateGetChapters = (req: Request, res: Response, next: NextFunction) => {
+  const includesChapter: GetChapterRequest['query']['include'] = [
+    'emptyPages',
+    'futurePublishAt',
+    'externalUrl',
+  ];
+
   const querySchema = Joi.object<GetChapterRequest['query']>({
-    include: Joi.string().valid(includesChapter).required(),
-    exclude: Joi.string().valid(includesChapter).required(),
+    include: Joi.string().valid(...includesChapter),
+    exclude: Joi.string().valid(...includesChapter),
   });
   const paramsSchema = Joi.object<GetChapterRequest['params']>({
     id: Joi.string(),
   });
 
-  const options: ValidationOptions = {
-    abortEarly: false,
-    stripUnknown: true,
-  };
-
-  const validatedQueryResult = querySchema.validate(req.query, options);
-  const validatedParamsResult = paramsSchema.validate(req.params, options);
+  const validatedQueryResult = querySchema.validate(req.query);
+  const validatedParamsResult = paramsSchema.validate(req.params);
 
   if (validatedQueryResult.error) {
     return res
