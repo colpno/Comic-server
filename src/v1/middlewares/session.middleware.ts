3bd225d1fa03b14e642356585fe0466595bad9d1
Colpno <gvinhh@gmail.com>
2024-10-30 01:07:12 +0700

    store session in mongodb

--- src/v1/middlewares/session.middleware.ts
+++ src/v1/middlewares/session.middleware.ts
@@ -1,6 +1,7 @@
 import { config } from 'dotenv';
 import session from 'express-session';
 
+import { SessionStore } from '../../databases/MongoDB.database';
 import cookieConfig from '../configs/cookie.conf';
 import { Error500 } from '../utils/error.utils';
 
@@ -12,11 +13,17 @@ if (!COOKIE_SECRET) {
   throw new Error500('COOKIE_SECRET is not defined');
 }
 
+const sessionStore = new SessionStore(session).getStore();
+sessionStore?.on('error', (error) => {
+  throw new Error500(error);
+});
+
 const middleware = session({
   secret: COOKIE_SECRET,
   cookie: cookieConfig,
   resave: false,
   saveUninitialized: false,
+  store: sessionStore,
 });
 
 export default middleware;
