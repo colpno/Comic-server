c7476e742364eafb27f4ef61c8a91b1b10a2a2d3
Colpno <gvinhh@gmail.com>
2024-10-30 18:25:35 +0700

    hardcode the base endpoint

--- src/v1/configs/common.conf.ts
+++ src/v1/configs/common.conf.ts
@@ -2,10 +2,8 @@ import { config } from 'dotenv';
 
 config();
 
-export const API_VERSION = 'v1';
-export const BASE_ENDPOINT = process.env.BASE_ENDPOINT
-  ? `${process.env.BASE_ENDPOINT}/${API_VERSION}`
-  : `/api/${API_VERSION}`;
+export const API_VERSION = 'v1' as const;
+export const BASE_ENDPOINT = `/api/${API_VERSION}` as const;
 
 /** Separate multiple origins with a space. */
 export const CORS_ORIGINS = process.env.CORS_ORIGINS || 'http://localhost:5173';
