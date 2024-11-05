b45970bbc922e6c5a735d5586f701998d1dcd2af
Colpno <gvinhh@gmail.com>
2024-11-05 20:45:48 +0700

    add type that merges mongo fields with generic type

--- src/v1/types/common.type.ts
+++ src/v1/types/common.type.ts
@@ -1,10 +1,12 @@
 export type PrimitiveType = string | number | boolean;
+
+/** Retrieve all values of a type. */
+export type ValueOf<T> = T[keyof T];
+
 export interface MongoDocFields {
   _id: string;
   __v: number;
   createdAt: string;
   updatedAt: string;
 }
-
-/** Retrieve all values of a type. */
-export type ValueOf<T> = T[keyof T];
+export type MongoDoc<T> = MongoDocFields & T;
