51d7c20430a06a4ee05506b67b64f467abc53806
Colpno <gvinhh@gmail.com>
2024-11-05 20:09:55 +0700

    add environment of the whole app

--- src/v1/configs/common.conf.ts
+++ src/v1/configs/common.conf.ts
@@ -2,5 +2,7 @@ import { config } from 'dotenv';
 
 config();
 
+export const APP_ENVIRONMENT = process.env.NODE_ENV || 'production';
+
 /** Separate multiple origins with a space. */
 export const CORS_ORIGINS = process.env.CORS_ORIGINS || 'http://localhost:5173';
