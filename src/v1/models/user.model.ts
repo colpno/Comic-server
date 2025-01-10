79053db6d49ad17cc7779d7dffea624ba76504eb
Colpno <gvinhh@gmail.com>
2025-01-10 21:28:09 +0700

    type the model

--- src/v1/models/user.model.ts
+++ src/v1/models/user.model.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 
-import { User } from '../types/user.type';
+import { User as OriUser } from '../types/user.type';
+
+type User = Omit<OriUser, 'id'>;
 
 const model = new mongoose.Schema<User>({
   email: { type: String, required: true, unique: true },
@@ -8,8 +10,9 @@ const model = new mongoose.Schema<User>({
     hashed: { type: String, required: true },
     salt: { type: String, required: true },
   },
+  refreshToken: { type: String },
 });
 
-const UserModel = mongoose.model('users', model);
+const UserModel = mongoose.model<User>('users', model);
 
 export default UserModel;
