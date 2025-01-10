7049be9adfcc579684e2bef20c5c1b5814faca61
Colpno <gvinhh@gmail.com>
2025-01-10 21:14:43 +0700

    create a function to generate salt for hashing

--- src/v1/utils/crypto.ts
+++ src/v1/utils/crypto.ts
@@ -4,11 +4,11 @@ export const generateCSRFToken = () => {
   return randomBytes(20).toString('hex');
 };
 
-export const hashString = (
-  plainText: string,
-  salt: string = randomBytes(16).toString('hex'),
-  algorithm: string = 'sha256'
-) => {
+export const generateSalt = (length: number = 16) => {
+  return randomBytes(length).toString('hex');
+};
+
+export const hashString = (plainText: string, salt: string, algorithm: string = 'sha256') => {
   const hash = createHash(algorithm);
   hash.update(plainText + salt);
   const hashedValue = hash.digest('hex');
