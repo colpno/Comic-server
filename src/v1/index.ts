9d2b074f795db000aa43b39811e126b50b68eb49
Colpno <gvinhh@gmail.com>
2025-01-10 21:11:04 +0700

    replace csrf-csrf package with csurf

--- src/v1/index.ts
+++ src/v1/index.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 
 import { BASE_ENDPOINT } from './constants/common.constant';
-import { cookieParser, cors, errorHandler, session } from './middlewares';
+import { cookieParser, cors, errorHandler } from './middlewares';
 import router from './routes';
 
 const v1App = express();
 
 v1App.use(cors);
-v1App.use(session);
 v1App.use(cookieParser);
 
 v1App.use(BASE_ENDPOINT, router);
--- src/v1/middlewares/index.ts
+++ src/v1/middlewares/index.ts
@@ -1,6 +1,4 @@
 export { default as cookieParser } from './cookie.middleware';
 export { default as cors } from './cors.middleware';
-export { doubleCsrfProtection } from './csrf.middleware';
 export { default as errorHandler } from './errorHandler.middleware';
-export { default as session } from './session.middleware';
 export { default as verifyAccessToken } from './verifyAccessToken.middleware';
