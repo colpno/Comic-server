be339ea5d54d33778e78b4ff6b75b71c354dfa4f
Colpno <gvinhh@gmail.com>
2025-01-12 16:58:58 +0700

    add mangadex controller

--- src/v1/controllers/index.ts
+++ src/v1/controllers/index.ts
@@ -2,3 +2,4 @@ export * as authController from './auth.controller';
 export * as chapterController from './chapter.controller';
 export * as comicController from './comic.controller';
 export * as followController from './follow.controller';
+export * as mangadexController from './mangadex.controller';
