067874b8c9b4b98d8eeb0d9ab47de7f6445f99c4
Colpno <gvinhh@gmail.com>
2025-01-13 17:08:51 +0700

    add missing schema keys in getComicList

--- src/v1/validations/comic.validation.ts
+++ src/v1/validations/comic.validation.ts
@@ -68,7 +68,9 @@ export const getComicList = (req: Request, res: Response, next: NextFunction) =>
     createdAt: Joi.string().isoDate(),
     updatedAt: Joi.string().isoDate(),
     hasAvailableChapters: Joi.string().valid('true', 'false'),
+    includedTags: Joi.array().items(Joi.string()),
     includedTagsMode: Joi.string().valid(...allowedTagMode),
+    ids: Joi.array().items(Joi.string()),
   });
 
   const { error, value } = schema.validate(req.query, options);
