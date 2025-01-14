b21c73f03d3d3716a0d4b686cdc261b112ffb9a8
Colpno <gvinhh@gmail.com>
2025-01-14 20:14:36 +0700

    extract MangaListQuery schema to a file

--- src/v1/validations/comic.validation.ts
+++ src/v1/validations/comic.validation.ts
@@ -4,18 +4,14 @@ import { Schema, ValidationOptions } from 'joi';
 import { HTTP_400_BAD_REQUEST } from '../../constants/httpCode.constant';
 import { Joi } from '../configs/joi.conf';
 import { GetComicById, GetComics } from '../controllers/comic.controller';
-import { Comic } from '../types/comic.type';
 import { MangaListQuery } from '../types/mangadex.type';
 import { processValidationError, validateGetRequest } from '../utils/validation.util';
+import { mangadexMangaListSchema } from './variables';
 
 type GetComicListSchema = Record<keyof Parameters<GetComics>[0]['query'], Schema>;
 
 export const getComicList = (req: Request, res: Response, next: NextFunction) => {
   const { _select, _embed, _sort, ...commands } = validateGetRequest;
-  const allowedTypes: Comic['type'][] = ['manga', 'manhwa', 'manhua'];
-  const allowedStatuses: Comic['status'][] = ['ongoing', 'completed', 'hiatus', 'cancelled'];
-  const allowedContentRatings: Comic['contentRating'][] = ['safe', 'suggestive'];
-  const allowedTagMode: MangaListQuery['includedTagsMode'][] = ['AND', 'OR'];
   const allowedEmbeds: MangaListQuery['includes'] = [
     'author',
     'artist',
@@ -59,21 +55,9 @@ export const getComicList = (req: Request, res: Response, next: NextFunction) =>
         })
       )
     ),
-    type: Joi.string().valid(...allowedTypes),
-    title: Joi.string(),
-    status: Joi.array().items(Joi.string().valid(...allowedStatuses)),
-    year: Joi.number().integer().positive().greater(1900).less(2100),
-    contentRating: Joi.string().valid(...allowedContentRatings),
-    createdAt: Joi.string().isoDate(),
-    updatedAt: Joi.string().isoDate(),
-    hasAvailableChapters: Joi.string().valid('true', 'false'),
-    includedTags: Joi.array().items(Joi.string()),
-    includedTagsMode: Joi.string().valid(...allowedTagMode),
-    ids: Joi.array().items(Joi.string()),
-    excludedTags: Joi.array().items(Joi.string()),
-    excludedTagsMode: Joi.string().valid(...allowedTagMode),
     _limit: Joi.number().integer().positive().max(100),
     _page: Joi.number().integer().positive(),
+    ...mangadexMangaListSchema,
   });
 
   const { error, value } = schema.validate(req.query, options);
---
+++ src/v1/validations/variables.ts
@@ -0,0 +1,28 @@
+import { Schema } from 'joi';
+
+import { Joi } from '../configs/joi.conf';
+import { Comic } from '../types/comic.type';
+import { MangaListQuery } from '../types/mangadex.type';
+
+const allowedTypes: Comic['type'][] = ['manga', 'manhwa', 'manhua'];
+const allowedStatuses: Comic['status'][] = ['ongoing', 'completed', 'hiatus', 'cancelled'];
+const allowedContentRatings: Comic['contentRating'][] = ['safe', 'suggestive'];
+const allowedTagMode: MangaListQuery['includedTagsMode'][] = ['AND', 'OR'];
+export const mangadexMangaListSchema: Record<
+  keyof Omit<MangaListQuery, 'limit' | 'offset' | 'order' | 'includes'>,
+  Schema
+> = {
+  type: Joi.string().valid(...allowedTypes),
+  title: Joi.string(),
+  status: Joi.array().items(Joi.string().valid(...allowedStatuses)),
+  year: Joi.number().integer().positive().greater(1900).less(2100),
+  contentRating: Joi.string().valid(...allowedContentRatings),
+  createdAt: Joi.string().isoDate(),
+  updatedAt: Joi.string().isoDate(),
+  hasAvailableChapters: Joi.string().valid('true', 'false'),
+  includedTags: Joi.array().items(Joi.string()),
+  includedTagsMode: Joi.string().valid(...allowedTagMode),
+  ids: Joi.array().items(Joi.string()),
+  excludedTags: Joi.array().items(Joi.string()),
+  excludedTagsMode: Joi.string().valid(...allowedTagMode),
+};
