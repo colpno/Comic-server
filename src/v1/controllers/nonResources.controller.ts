ac8774445d1703d64c7da7ec39140834d76842e8
Colpno <gvinhh@gmail.com>
2025-01-23 19:44:16 +0700

    make healthCheck always return with status 200

--- src/v1/controllers/nonResources.controller.ts
+++ src/v1/controllers/nonResources.controller.ts
@@ -1,21 +1,19 @@
 import axios from 'axios';
 import { RequestHandler } from 'express';
 
-import { HTTP_503_SERVER_UNAVAILABLE } from '../../constants/httpCode.constant';
-
 type HealthStatus = 'ok' | 'maintenance';
 type HealthCheck = RequestHandler<{}, { status: HealthStatus }, null>;
 
-export const healthCheck: HealthCheck = async (_req, res, next) => {
+export const healthCheck: HealthCheck = async (_req, res) => {
   try {
     const response = await axios.get('https://api.mangadex.org/ping');
 
     if (response.status === 200 && response.data === 'pong') {
       res.json({ status: 'ok' });
     } else {
-      res.status(HTTP_503_SERVER_UNAVAILABLE).json({ status: 'maintenance' });
+      res.json({ status: 'maintenance' });
     }
   } catch (error) {
-    next(error);
+    res.json({ status: 'maintenance' });
   }
 };
