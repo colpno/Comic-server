32fd4e5e51d1e73b509c997411478c0ea58e1788
Colpno <gvinhh@gmail.com>
2025-01-24 16:07:52 +0700

    add host variables

--- dist/configs/app.conf.js
+++ dist/configs/app.conf.js
@@ -1,7 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.PORT = void 0;
+exports.HOST_URL = exports.PORT = exports.HOST = void 0;
 const dotenv_1 = require("dotenv");
+const common_constant_1 = require("../v1/constants/common.constant");
 (0, dotenv_1.config)();
+exports.HOST = process.env.HOST || 'localhost';
 exports.PORT = process.env.PORT || 3000;
+exports.HOST_URL = process.env.HOST_URL
+    ? `${process.env.HOST_URL}${common_constant_1.BASE_ENDPOINT}`
+    : `http://${exports.HOST}:${exports.PORT}${common_constant_1.BASE_ENDPOINT}`;
 //# sourceMappingURL=app.conf.js.map
--- src/configs/app.conf.ts
+++ src/configs/app.conf.ts
@@ -1,5 +1,11 @@
 import { config } from 'dotenv';
 
+import { BASE_ENDPOINT } from '../v1/constants/common.constant';
+
 config();
 
+export const HOST = process.env.HOST || 'localhost';
 export const PORT = process.env.PORT || 3000;
+export const HOST_URL = process.env.HOST_URL
+  ? `${process.env.HOST_URL}${BASE_ENDPOINT}`
+  : `http://${HOST}:${PORT}${BASE_ENDPOINT}`;
