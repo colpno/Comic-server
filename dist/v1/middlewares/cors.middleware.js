38bc2812c06a50f00d35642dbf7b6bec8f7c7c63
Colpno <gvinhh@gmail.com>
2025-01-24 17:41:25 +0700

    properly config dynamic origin

--- dist/v1/middlewares/cors.middleware.js
+++ dist/v1/middlewares/cors.middleware.js
@@ -5,8 +5,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const cors_1 = __importDefault(require("cors"));
 const common_conf_1 = require("../configs/common.conf");
+const error_utils_1 = require("../utils/error.utils");
+const whitelistRegex = common_conf_1.CORS_ORIGINS.split(' ').map((origin) => new RegExp(origin));
 const middleware = (0, cors_1.default)({
-    origin: common_conf_1.CORS_ORIGINS.split(' '),
+    origin: function (origin, callback) {
+        const isAllowed = whitelistRegex.some((regex) => regex.test(origin)) || !origin;
+        if (isAllowed) {
+            callback(null, true);
+        }
+        else {
+            callback(new error_utils_1.Error403('Not allowed by CORS'));
+        }
+    },
     credentials: true,
 });
 exports.default = middleware;
--- src/v1/middlewares/cors.middleware.ts
+++ src/v1/middlewares/cors.middleware.ts
@@ -1,9 +1,20 @@
 import cors from 'cors';
 
 import { CORS_ORIGINS } from '../configs/common.conf';
+import { Error403 } from '../utils/error.utils';
+
+const whitelistRegex = CORS_ORIGINS.split(' ').map((origin) => new RegExp(origin));
 
 const middleware = cors({
-  origin: CORS_ORIGINS.split(' '),
+  origin: function (origin, callback) {
+    const isAllowed = whitelistRegex.some((regex) => regex.test(origin)) || !origin;
+
+    if (isAllowed) {
+      callback(null, true);
+    } else {
+      callback(new Error403('Not allowed by CORS'));
+    }
+  },
   credentials: true,
 });
 
